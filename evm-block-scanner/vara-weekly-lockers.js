de0f243757f19ac8ae1afc11352d70bff50176d7
wendel <wendel@equilibrefinance.com>
2023-03-16 13:35:07 -0300

    vara-weekly-lockers data for review.

--- evm-block-scanner/vara-weekly-lockers.js
+++ evm-block-scanner/vara-weekly-lockers.js
@@ -1,14 +1,13 @@
 'use strict'
 const rpcArchive = 'https://evm.data.equilibre.kava.io';
 const fs = require('fs');
+const moment = require('moment');
 const Web3 = require('web3');
 const web3 = new Web3(rpcArchive);
 
 const votingEscrowContract = '0x35361C9c2a324F5FB8f3aed2d7bA91CE1410893A';
 const bribeContract = '0xc401adf58F18AF7fD1bf88d5a29a203d3B3783B2';
 const minAmount = 165;
-
-let BLOCK_START = 0, BLOCK_END = 0;
 
 let address = [], info = [], totalVARA = 0, totalVE = 0;
 const abi = JSON.parse(fs.readFileSync("./voting-escrow-abi.js", "utf8"));
@@ -33,14 +32,12 @@ function computeVeVARA(amount, locktime, ts){
     const iMAXTIME = 4 * YEAR * DAY
     const slope = amount / iMAXTIME;
     const bias = slope * (locktime - ts) * years;
-    // const multiplier = parseFloat(bias/days).toFixed(2);
-    // console.log('amount', amount, 'days', days, 'reward', bias, 'years', years, 'multiplier', multiplier);
     return parseFloat(bias);
 }
 
 async function onNewEvent(error, events){
     if (error) {
-        console.log(error);
+        console.log(error.toString());
     } else {
         for (let j = 0; j < events.length; j++) {
             const e = events[j];
@@ -49,7 +46,8 @@ async function onNewEvent(error, events){
             const u = e.returnValues;
             const amount = parseFloat(web3.utils.fromWei(u.value));
             const ve = await computeVeVARA(amount, parseInt(u.locktime), parseInt(u.ts));
-            const line = `${u.provider}, VARA: ${amount}, veVARA: ${ve}`;
+            const days = parseInt(( u.locktime - u.ts) / DAY);
+            const line = `${u.provider}, VARA: ${amount}, veVARA: ${ve}, days: ${days}`;
             if( u.locktime < epoch ){
                 console.log(` DISCARD: ${line}`);
                 continue;
@@ -98,20 +96,32 @@ async function scanBlockchain(start, end) {
     fs.writeFileSync('../vara-weekly-lockers.md', info.join('\n') );
     fs.writeFileSync('../vara-weekly-lockers.csv', lines.join('\n') );
 }
-async function getEpochBlock(){
+async function getBlocksFromLastEpoch(){
+    let BLOCK_START = 0, BLOCK_END = 0;
     BLOCK_END = parseInt(await web3.eth.getBlockNumber());
-    const latestBlock = await web3.eth.getBlock("latest");
-    epoch = await bribe.methods.getEpochStart(latestBlock.timestamp).call();
-    console.log(`epoch=${epoch}`);
+    let latestBlock = await web3.eth.getBlock("latest");
+    epoch = parseInt((await bribe.methods.getEpochStart(latestBlock.timestamp).call()).toString());
     for(let i = BLOCK_END; i > 0 ; i -= 10000 ){
         const currentBlock = await web3.eth.getBlock(i);
-        console.log(`${i} ${currentBlock.timestamp} ${epoch}`);
         if( currentBlock.timestamp <= epoch){
-            BLOCK_START = i;
-            console.log(`BLOCK_START=${BLOCK_START}`);
+            BLOCK_END = i;
+            epoch = parseInt(currentBlock.timestamp);
             break;
         }
     }
+    BLOCK_START = BLOCK_END-86400;
+    latestBlock = await web3.eth.getBlock(BLOCK_START);
+    epoch = parseInt((await bribe.methods.getEpochStart(latestBlock.timestamp).call()).toString());
+    for(let i = BLOCK_START; i > 0 ; i -= 10000 ){
+        const currentBlock = await web3.eth.getBlock(i);
+        if( currentBlock.timestamp <= epoch){
+            BLOCK_START = i;
+            epoch = parseInt(currentBlock.timestamp);
+            break;
+        }
+    }
+    console.log(`BLOCK_START=${BLOCK_START}, BLOCK_END=${BLOCK_END}`)
+    return {BLOCK_START, BLOCK_END}
 }
 async function main() {
 
@@ -119,12 +129,17 @@ async function main() {
     // computeVeVARA(360, 1678318983+(YEAR*DAY*2), 1678318983);
     // computeVeVARA(360, 1678318983+(YEAR*DAY*4), 1678318983);
     // await scanByBlock(3897052);
+    let blocks;
+    try {
+        blocks = await getBlocksFromLastEpoch();
+    }catch(e){
+        console.log(`Error building {BLOCK_START, BLOCK_END}: ${e.toString()}`);
+    }
 
-    await getEpochBlock();
-    if( BLOCK_END && BLOCK_START ){
-        await scanBlockchain(BLOCK_START, BLOCK_END);
-    }else{
-        console.log('error in blocks start/end.');
+    try {
+        await scanBlockchain(blocks.BLOCK_START, blocks.BLOCK_END);
+    }catch(e){
+        console.log(`Error running the chain scan: ${e.toString()}`);
     }
 
 }
